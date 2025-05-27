e6acfc5525638e4816c2b1629c1190040ec8e789
Anty2767 <antonio.dibari2767@gmail.com>
2025-05-27 10:01:40 +0200

    aggiunta main, leggidouble e leggistringa

---
+++ main.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+class Main{
+    public static void main(String args[]){
+        double dato1=leggiDouble("inserisci 1^ numero");
+        double dato2=leggiDouble("inserisci 2^ numero");
+        System.out.println(sottrai(dato1,dato2));
+        System.out.println(addizione(dato1,dato2));
+        System.out.println(dividi(dato1,dato2));
+    }
+
+    public static double leggiDouble(String domanda){//leggi intero POSITIVO
+    double numeroInserito = 0;
+    boolean conversione=false;//flag che controlla che la conversione sia corretta
+    do{
+      try{
+        numeroInserito=Double.parseDouble(leggiStringa(domanda));
+        conversione=true;
+      }catch(ArithmeticException e){
+        System.out.println("devi inserire un numero! "+e+"\n");
+      }catch(Exception e){
+        System.out.println("errore nella conversione "+e+"\n");
+      }
+    }while(!conversione);
+    return numeroInserito;
+  }
+
+/**
+   * Legge una stringa da input.
+   *
+   * @param domanda La domanda da visualizzare.
+   * @return La stringa inserita.
+   */
+  public static String leggiStringa(String domanda) {//leggi STRINGA
+    BufferedReader k=new BufferedReader (new InputStreamReader(System.in));
+    boolean corretto=false; //flag che controlla che l'input sia corretto
+    String risposta = "";
+    do{
+      try{
+        System.out.println(domanda);
+        risposta = k.readLine();
+        corretto=true;
+      }catch(IOException e){
+        System.out.println("errore nell'acquisizione del dato!\n");
+      }catch(Exception e){
+        System.out.println("errore "+e+"\n");
+      }
+    }while(!corretto);
+    return risposta;
+  }  
+}
